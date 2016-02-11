0ffcd5e26d209ffa150b2190987d51e933f82046
Robert Gabriel <robert_gabriel@outlook.com>
2016-02-11 18:35:45 +0000

    Removed old examples

--- datacenter/src/paint/Painter.java
+++
@@ -1,8 +0,0 @@
-package paint;
-
-/**
- * Created by toni on 09/02/16.
- */
-public interface Painter {
-    void paint();
-}
--- datacenter/src/reader/IllegalFileException.java
+++
@@ -1,7 +0,0 @@
-package reader;
-
-/**
- * Created by toni on 09/02/16.
- */
-public class IllegalFileException extends Exception {
-}
