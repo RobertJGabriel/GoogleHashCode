078bb68b67a5c947d3de6a8fac5145e96291e0bd
Robert Gabriel <robert_gabriel@outlook.com>
2016-02-11 18:02:54 +0000

    Added Main.java

---
+++ Drone/src/Main.java
@@ -0,0 +1,12 @@
+
+/**
+ * Created by drop tables--; on 11/02/16.
+ */
+public class Main {
+
+    public static void main(String... args){
+
+        System.out.println("lets go buddys");
+    }
+
+}
